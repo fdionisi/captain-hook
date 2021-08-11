5aeb5e3a1e963fd5b75908326799660dcdca314f
Federico Dionisi <code@fdionisi.me>
2021-08-11 17:57:09 +0200

    refactor: simplify usage

--- src/main.rs
+++ src/main.rs
@@ -8,16 +8,13 @@ use crate::error::Error;
 const USAGE: &'static str = r#"captain-hook 
 
 USAGE:
-    captain-hook <SUBCOMMAND>
+    captain-hook install [dir] (default: .hooks)
+    captain-hook add <file> <cmd>
+    captain-hook uninstall
 
 FLAGS:
     -h, --help       Prints help information
     -V, --version    Prints version information
-
-SUBCOMMANDS:
-    install      
-    add          
-    uninstall
 "#;
 
 fn print_usage() -> Result<(), Error> {
@@ -31,28 +28,23 @@ fn print_version() -> Result<(), Error> {
 }
 
 fn main() -> Result<(), Error> {
-    let mut args: Vec<String> = env::args().collect();
-    let _ = args.remove(0);
-    if args.len() == 0 {
+    let args: Vec<String> = env::args().collect();
+    if args.len() == 1 {
         return print_usage();
     }
 
-    let sub_command = args.remove(0);
+    let sub_command = args.get(1).unwrap();
     match sub_command.as_str() {
         "install" => {
             let temp_dir = String::from(".hooks");
-            let dir = args.get(0).unwrap_or(&temp_dir);
+            let dir = args.get(2).unwrap_or(&temp_dir);
             commands::install(&dir)
         }
         "uninstall" => commands::uninstall(),
-        "add" => {
-            let file_name = args.get(0);
-            let cmd = args.get(1);
-            match (file_name, cmd) {
-                (Some(f), Some(cmd)) => commands::add(&f, &cmd),
-                _ => print_usage(),
-            }
-        }
+        "add" => match (args.get(2), args.get(3)) {
+            (Some(file_name), Some(cmd)) => commands::add(&file_name, &cmd),
+            _ => print_usage(),
+        },
         "--version" | "-V" => print_version(),
         "--help" | "-h" => print_usage(),
         _ => print_usage(),
